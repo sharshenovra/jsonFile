1d31bdbcc03686ae509106bbd94f1fa88e69d9fc
sharshenovra <67783261+sharshenovra@users.noreply.github.com>
2022-01-18 19:32:35 +0600

    Initial Commit

---
+++ jsonFile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jsonFile
+//
+//  Created by Ruslan Sharshenov on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
